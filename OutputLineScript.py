982493f0fe2a1d4e3af6460662232ac5330127fc
Albert Z Liao <albert.s.mailbox.hahaha@gmail.com>
2018-08-13 11:00:16 -0700

    fixes

--- OutputLineScript.py
+++ OutputLineScript.py
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-#this program adds a log output line, a .ses output line, log dir, sess dir, and a quit command in the .scmd file of each test case.
-#it will check if each .scmd file in the each test case has the aforementioned three lines then add them as needed 
+#this program adds a log output line, a .ses output line, log dir, sess dir, a
+#quit command, enable output in the .scmd file of each test case.
+#it will check if each .scmd file in the each test case has the aforementioned
+#three lines then add them as needed 
 #Albert Liao, 6/27/2018 
 import os #using os.listdir, os.path.dirname, os.path.abspath, os.chdir...
+
+def strChk(a):#checks if a given string starts with ses_output or route_report_output
+    sesOP = False
+    logOP = False
+
+    if (a.startswith('ses_output')):
+        sesOP = True
+    if(a.startswith('route_report_output')):
+        logOP = True
+    return sesOP, logOP
 
 DirNArray = os.listdir() #turning all of the test case files into an array
 subDirArray = os.listdir() #testcase directory name array  
@@ -15,12 +27,17 @@ sessBools = False
 for i in range(len(DirNArray)):
     logBools = False 
     sessBools = False
-    subDir = os.path.join(address,DirNArray[i])#appends the current directory address with a subdirectory name to form the address for the sub directory
+
+    #appends the current directory address with a subdirectory name to form the
+    #address for the sub directory
+    subDir = os.path.join(address,DirNArray[i])
+
     if( os.path.isdir(subDir) != True): 
         continue 
     os.chdir(subDir)#change the current working directory to one of the subdir
 
-    #listing the names of the entries in the directory to find the directory has logFile and sesFile 
+    #listing the names of the entries in the directory to find the directory 
+    #has logFile and sesFile 
     subDirArray = os.listdir()
 
     for i in range(len(subDirArray)):
@@ -35,3 +52,6 @@ for i in range(len(DirNArray)):
 
     if(sessBools == False):
         os.makedirs('sesFile')
+
+
+
